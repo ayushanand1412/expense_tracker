70682275f345756cb657eea54039eaf32356843d
Ayush Anand <ayushanand4814@gmail.com>
2025-06-26 12:01:01 +0530

    Add custom User model and manager using email as username

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    # profile_picture = models.ImageField(upload_to='users/', null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
